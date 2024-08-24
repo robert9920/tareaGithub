ea7ca8f8c99c2f46f7ae3b22517c19bf4c836789
robertp9920 <roberthumberto2008@hotmail.com>
2024-08-23 21:34:56 -0500

    Se resolvio Fixes #1

---
+++ Tarea_S4_Robert_Lesly/exploracionDx.py
@@ -0,0 +1,37 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def corrVar(df):
+    print("Su dataframe presenta las siguientes variables del tipo cuantitativo:")
+    columns_num = df.select_dtypes("number").columns.tolist()
+    print(columns_num)
+
+    print("Escriba el nombre de la primera variable numérica deseada")
+    col1 = input()
+
+    print("\n")
+    
+    while (col1 not in columns_num):
+        print("Nombre de variable no válido, por favor seleccionar una variable disponible: ")
+        col1 = input()
+
+    print("Ahora escriba el nombre de la segunda variable para conocer la correlación con la primera: ")
+    col2 = input()
+
+    while (col2 not in columns_num):
+        print("Nombre de variable no válido, por favor seleccionar la segunda variable numeria deseada:  ")
+        col2 = input()
+
+
+    corr = df[col1].corr(df[col2])
+
+    print(f"La correlacion entre la variable 1 y 2 es: {corr:.4f}")
+
+
+def graficar_pairplot(data, hue=None, palette = "Set2", filename="pairplot.png"):
+    pairplot = sns.pairplot(data, hue = hue, palette = palette)
+    plt.show()
+
+
+def func_prueba():
+    print("Hola Mundo")
